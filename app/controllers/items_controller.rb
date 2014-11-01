2abafc195fba0e97d96cfa4fb0e92871410db6df
Danielle Gellis <danisyellis@gmail.com>
2014-11-01 15:03:34 -0700

    can add items to to-do lists, also make home and about links in header

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,26 @@ class ItemsController < ApplicationController
   def index
     @todo_list = TodoList.find(params[:todo_list_id])
   end
+
+  def new
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @item = @todo_list.items.new
+  end
+
+  def create
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @item = @todo_list.items.new(item_params)
+    if @item.save
+      flash[:success] = "Added to-do list item."
+      redirect_to todo_list_items_path
+    else
+      flash[:error] = "There was a problem adding that item"
+      render action: :new
+    end
+  end
+
+  private
+  def item_params
+    params[:item].permit(:content)
+  end
 end
