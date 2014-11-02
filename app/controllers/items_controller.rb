fe804f6cff51fffceafd071987f877e185390ca6
Danielle Gellis <danisyellis@gmail.com>
2014-11-01 17:21:12 -0700

    add flash message div styling, add ability to edit items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,11 +12,28 @@ class ItemsController < ApplicationController
     @todo_list = TodoList.find(params[:todo_list_id])
     @item = @todo_list.items.new(item_params)
     if @item.save
-      flash[:success] = "Added to-do list item."
+      flash[:success] = "Added to-do list item"
       redirect_to todo_list_items_path
     else
       flash[:error] = "There was a problem adding that item"
       render action: :new
+    end
+  end
+
+  def edit
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @item = @todo_list.items.find(params[:id])
+  end
+
+  def update
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @item = @todo_list.items.find(params[:id])
+    if @item.update_attributes(item_params)
+      flash[:success] = "Saved todo list item"
+      redirect_to todo_list_items_path
+    else
+      flash[:error] = "There was a problem saving that item"
+      render action: :edit
     end
   end
 
