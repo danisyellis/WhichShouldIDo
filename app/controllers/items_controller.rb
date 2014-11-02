817d982b1afe6c0e5c43bd44be2c981437f8d3f3
Danielle Gellis <danisyellis@gmail.com>
2014-11-01 18:30:54 -0700

    a little bit of refactoring

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,15 @@
 class ItemsController < ApplicationController
+  before_action :find_todo_list
+
   def index
-    @todo_list = TodoList.find(params[:todo_list_id])
+
   end
 
   def new
-    @todo_list = TodoList.find(params[:todo_list_id])
     @item = @todo_list.items.new
   end
 
   def create
-    @todo_list = TodoList.find(params[:todo_list_id])
     @item = @todo_list.items.new(item_params)
     if @item.save
       flash[:success] = "Added to-do list item"
@@ -21,12 +21,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @todo_list = TodoList.find(params[:todo_list_id])
     @item = @todo_list.items.find(params[:id])
   end
 
   def update
-    @todo_list = TodoList.find(params[:todo_list_id])
     @item = @todo_list.items.find(params[:id])
     if @item.update_attributes(item_params)
       flash[:success] = "Saved todo list item"
@@ -37,7 +35,13 @@ class ItemsController < ApplicationController
     end
   end
 
+
   private
+
+  def find_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
   def item_params
     params[:item].permit(:content)
   end
